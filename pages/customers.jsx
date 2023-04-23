93e80afc883607666777b3caabaaa2ab60363e4b
John Pertuz <johnpertuzsism@gmail.com>
2023-04-22 19:37:49 -0500

    CREACION DE LAS RUTAS CUSTOMERS Y ORDERS

---
+++ pages/customers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BsPersonFill, BsThreeDotsVertical } from 'react-icons/bs'
+import { data } from '../data/data'
+
+const customers = () => {
+  return (
+    <div>customers</div>
+  )
+}
+
+export default customers
